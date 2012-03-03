d182b7ce7a535b62805c0e875187ab1d9a2da3d6
MiracleM4n <MiracleM4n@gmail.com>
2012-03-03 12:28:15 -0500

    Remove Second Pass code.

--- src/main/java/in/mDev/MiracleM4n/mChatSuite/api/mChatAPI.java
+++ src/main/java/in/mDev/MiracleM4n/mChatSuite/api/mChatAPI.java
@@ -640,30 +640,17 @@ public class mChatAPI {
     }
 
     String replaceVars(String format, Map<String, Object> map) {
-        //First Pass
         for (Entry<String, Object> entry : map.entrySet()) {
-            if (entry.getKey().contains(","))
+            if (entry.getKey().contains(",")) {
                 for (String s : entry.getKey().split(",")) {
                     if (s == null || entry.getValue() == null)
                         continue;
 
                     format = format.replace(s, entry.getValue().toString());
                 }
-            else
+            } else {
                 format = format.replace(entry.getKey(), entry.getValue().toString());
-        }
-
-        //Second Pass
-        for (Entry<String, Object> entry : map.entrySet()) {
-            if (entry.getKey().contains(","))
-                for (String s : entry.getKey().split(",")) {
-                    if (s == null || entry.getValue() == null)
-                        continue;
-
-                    format = format.replace(s, entry.getValue().toString());
-                }
-            else
-                format = format.replace(entry.getKey(), entry.getValue().toString());
+            }
         }
 
         return addColour(format);
