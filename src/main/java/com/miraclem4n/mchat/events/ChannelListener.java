b75e336c80f8d9ec260376c47088e02681a2e648
MiracleM4n <MiracleM4n@gmail.com>
2012-08-12 01:57:09 -0400

    Update to AsyncChatEvent.

--- src/main/java/com/miraclem4n/mchat/events/ChannelListener.java
+++ src/main/java/com/miraclem4n/mchat/events/ChannelListener.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.Set;
@@ -21,7 +21,7 @@ public class ChannelListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOW)
-    public void onPlayerChat(PlayerChatEvent event) {
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
         if (event.isCancelled())
             return;
 
--- src/main/java/com/miraclem4n/mchat/events/ChatListener.java
+++ src/main/java/com/miraclem4n/mchat/events/ChatListener.java
@@ -11,7 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 import java.util.Date;
@@ -25,7 +25,7 @@ public class ChatListener implements Listener {
 
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerChat(PlayerChatEvent event) {
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
         if (event.isCancelled())
             return;
 
