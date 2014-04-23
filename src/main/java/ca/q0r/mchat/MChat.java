ef4853cec54c1852708db4333eb51f0f09b9ebee
MiracleM4n <MiracleM4n@gmail.com>
2014-04-23 19:03:01 -0400

    [Test] Test AWS broken build uploading.

--- src/main/java/ca/q0r/mchat/MChat.java
+++ src/main/java/ca/q0r/mchat/MChat.java
@@ -31,6 +31,8 @@ public class MChat extends JavaPlugin {
     public PluginManager pm;
     public PluginDescriptionFile pdfFile;
 
+    Test AWS Breakage.
+
     public Boolean update = false;
 
     public void onEnable() {
